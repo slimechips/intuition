836c6fcba319dc9453339e4611609c62ebb25fa5
slimechips <jason.chowcs#gmail.com>
2019-10-13 01:31:13 +0800

    fix error handling

--- common-util/python.ts
+++ common-util/python.ts
@@ -3,11 +3,15 @@ import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
 export const runPython = (pythonFile: string, params: Array<string>): Promise<object> => {
   const pyS: ChildProcessWithoutNullStreams = spawn('python3',
     [pythonFile].concat(`"${params}"`));
-
   return new Promise((resolve, reject): void => {
     pyS.stdout.on('data', (data: Buffer) => {
-      const fData: object = JSON.parse(data.toString());
-      resolve(fData);
+      try {
+        const fData: object = JSON.parse(data.toString());
+        resolve(fData);
+      } catch (err) {
+        console.log(data.toString());
+        reject(err);
+      }
     });
 
     pyS.stderr.on('data', (err: Buffer) => {
